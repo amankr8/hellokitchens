23c71327b001773455ab34c2431b16ced6a9581d
amankr8 <amankumar141099@gmail.com>
2025-12-30 23:43:42 +0530

    code refactor

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
 import { MenuComponent } from './pages/menu/menu.component';
-import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [
-  { path: '', component: MenuComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-];
+export const routes: Routes = [{ path: '', component: MenuComponent }];
