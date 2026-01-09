dbeee1c5f3635662fb8133605fa67f72400a2555
amankr8 <amankumar141099@gmail.com>
2026-01-09 23:22:15 +0530

    code refactor

--- backend/server/src/main/java/com/flykraft/livemenu/exception/GlobalExceptionHandler.java
+++ backend/server/src/main/java/com/flykraft/livemenu/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +14,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponseDto(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(new ErrorResponseDto(HttpStatus.PAYLOAD_TOO_LARGE.value(), "File size exceeds the maximum allowed limit!"));
     }
 }
