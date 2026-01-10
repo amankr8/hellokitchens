259f40ebdd0ba739d89a057c05833d185a484f2a
amankr8 <amankumar141099@gmail.com>
2026-01-10 17:00:29 +0530

    code update

---
+++ frontend/src/app/enum/user-role.enum.ts
@@ -0,0 +1,5 @@
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  KITCHEN_OWNER = 'KITCHEN_OWNER',
+  USER = 'USER',
+}
--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -1,15 +1,19 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
 import { inject } from '@angular/core';
+import { UserRole } from '../enum/user-role.enum';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (!authService.isAuthenticated()) {
-    router.navigate(['/login']);
-    return false;
+  if (authService.isAuthenticated()) {
+    const decodedToken = authService.getDecodedToken();
+    if (decodedToken?.role === UserRole.KITCHEN_OWNER) {
+      return true;
+    }
   }
 
-  return true;
+  router.navigate(['/login']);
+  return false;
 };
