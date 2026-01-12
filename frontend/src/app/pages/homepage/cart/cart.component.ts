55bda2015b3bcb2410a053928f3585b1ab362435
amankr8 <amankumar141099@gmail.com>
2026-01-13 00:18:01 +0530

    code update

--- frontend/src/app/pages/homepage/cart/cart.component.ts
+++ frontend/src/app/pages/homepage/cart/cart.component.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UserService } from '../../../service/user.service';
 import { Icons } from '../../../utils/icons';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../../service/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -12,18 +13,21 @@ import { RouterLink } from '@angular/router';
 })
 export class CartComponent {
   userService = inject(UserService);
+  cartService = inject(CartService);
 
   user = this.userService.user;
   selectedAddressId = signal<number | null>(null);
 
-  // Assuming you have a cart items signal
-  cartItems = signal<any[]>([]);
+  cartItems = this.cartService.cartItems;
 
   subtotal = computed(() =>
-    this.cartItems().reduce((acc, item) => acc + item.price * item.quantity, 0)
+    this.cartItems().reduce(
+      (acc, item) => acc + item.menuItem.price * item.quantity,
+      0
+    )
   );
 
-  deliveryFee = signal(40); // Fixed or calculated
+  deliveryFee = signal(40);
   platformFee = signal(5);
 
   totalAmount = computed(
