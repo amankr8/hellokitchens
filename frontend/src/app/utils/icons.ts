7cd3575e234e3d0c14a1617e483a74046491c2df
amankr8 <amankumar141099@gmail.com>
2026-01-21 10:11:58 +0530

    code update

--- frontend/src/app/utils/icons.ts
+++ frontend/src/app/utils/icons.ts
@@ -41,6 +41,9 @@ import {
   faEye,
   faHistory,
   faFilter,
+  faUserAlt,
+  faUserAltSlash,
+  faUserCheck,
 } from '@fortawesome/free-solid-svg-icons';
 
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
@@ -48,7 +51,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 export const Icons = {
   filter: faFilter,
   history: faHistory,
-  user: faUser,
+  user: faUserCheck,
   eyeSlash: faEyeSlash,
   eye: faEye,
   bowl: faBowlRice,
